16c3bf52ccffd8a0344a181d96fd9e71b96244f7
Miguel Hernandez <michelangelo93@gmail.com>
2016-06-29 00:30:12 +0100

    Rtabmap launch for Ps4Eye in La Laguna

--- ws/ps4_ws/devel/_setup_util.py
+++ ws/ps4_ws/devel/_setup_util.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/home/migue/Workspace/catkin_ws/devel;/opt/ros/jade'.split(';')
+        CMAKE_PREFIX_PATH = '/home/migue/Workspace/ROS/ws/ps4_ws/devel;/home/migue/Workspace/catkin_ws/devel;/opt/ros/jade'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:

3ef571ed64bf64e21667e67f76936c07a4b8a321
Miguel Hernandez <michelangelo93@gmail.com>
2016-06-28 21:02:29 +0100

    import perenquen ps4eye package

---
+++ ws/ps4_ws/src/ps4eye/record.sh
@@ -0,0 +1,2 @@
+rosbag record /stereo/left/camera_info /stereo/right/camera_info /stereo/right/image_raw/compressed /stereo/left/image_raw/compressed /perenquen/odom /odom_combined /left_laser_filtered /right_laser_filtered /rear_laser_filtered /tf /laser_odom
+
